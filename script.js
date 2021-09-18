e402f8f66fd70e08bdb6774637f3b4bdda4d07c4
grisha18 <granta54321@gmail.com>
2021-09-18 19:00:28 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ const left = document.querySelector(".arrow-left");
 const right = document.querySelector(".arrow-right");
 
 const anotherBlogersImages = document.querySelector(".another-blogers__images");
+const burger = document.querySelector('.burger');
+
+const menuList = document.querySelector(".menu__list");
+
+
+burger.addEventListener('click', () =>{
+
+    menuList.classList.toggle('menu-shown');
+    burger.classList.toggle('burger-rotated');
+
+})
 
 const delta = 327;
 
